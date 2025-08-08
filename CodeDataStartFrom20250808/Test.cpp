8a29b4109d844308bd365d20395f325bb1ca466c
HerbertLu <3242473480@qq.com>
2025-08-08 21:22:42 +0800

    Where is the f**king collaboration inconsistency bug ?

---
+++ CodeDataStartFrom20250808/Test.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Test
+
+int main() {
+    #ifdef Test
+        freopen("file.in", "r", stdin);
+        freopen("file.out", "w", stdout);
+    #endif 
+    cout << "F**king bug, where are you ?" << endl;
+    return 0;
+
+}
